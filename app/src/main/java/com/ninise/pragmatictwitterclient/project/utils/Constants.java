d9690b59c9612879ffb90b7c3c656107328fd227
Ninise <nikitonze@hotmail.com>
2016-02-15 19:18:56 +0200

    Created mvc architecture

---
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.ninise.pragmatictwitterclient.project.utils;
+
+/**
+ * Created by ninise on 2/15/16.
+ */
+public class Constants {
+
+    public static final String TWITTER_KEY = "yHD550aeiH9aioD1ieQXLk7Zl";
+    public static final String TWITTER_SECRET = "hcKlsofRySiwjxeiTxnppzIIGeU2lGDPBbnG2FCvvvd4eJGeze";
+
+}
