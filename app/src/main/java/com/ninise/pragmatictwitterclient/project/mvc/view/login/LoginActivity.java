04092e982df6f396677137d27d995fc5df549b9a
Ninise <nikitonze@hotmail.com>
2016-02-16 20:09:15 +0200

    Added logout function with clear cookie

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvc/view/login/LoginActivity.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvc/view/login/LoginActivity.java
@@ -1,10 +1,16 @@
 package com.ninise.pragmatictwitterclient.project.mvc.view.login;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.Toast;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 
 import com.ninise.pragmatictwitterclient.R;
 import com.ninise.pragmatictwitterclient.project.utils.Constants;
@@ -12,20 +18,29 @@ import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 import io.fabric.sdk.android.Fabric;
 
+
 public class LoginActivity extends AppCompatActivity {
 
+    public static final String TAG = LoginActivity.class.getName();
+
     TwitterLoginButton loginButton;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.loginToolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+        setSupportActionBar(toolbar);
 
         TwitterAuthConfig authConfig = new TwitterAuthConfig(Constants.TWITTER_KEY, Constants.TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
@@ -37,7 +52,7 @@ public class LoginActivity extends AppCompatActivity {
                 TwitterSession session = result.data;
 
                 String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                Log.d("TwitterKit", "Login with Twitter success" + msg);
             }
             @Override
             public void failure(TwitterException exception) {
@@ -45,12 +60,43 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        Log.d(TAG, "Activity created");
+
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         loginButton.onActivityResult(requestCode, resultCode, data);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        logoutTwitter();
+    }
+
+    public void logoutTwitter() {
+        TwitterSession twitterSession = TwitterCore.getInstance().getSessionManager().getActiveSession();
+        if (twitterSession != null) {
+            ClearCookies(getApplicationContext());
+            Twitter.getSessionManager().clearActiveSession();
+            Twitter.logOut();
+        }
+    }
+
+    public static void ClearCookies(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            CookieManager.getInstance().removeAllCookies(null);
+            CookieManager.getInstance().flush();
+        } else {
+            CookieSyncManager cookieSyncMngr=CookieSyncManager.createInstance(context);
+            cookieSyncMngr.startSync();
+            CookieManager cookieManager=CookieManager.getInstance();
+            cookieManager.removeAllCookie();
+            cookieManager.removeSessionCookie();
+            cookieSyncMngr.stopSync();
+            cookieSyncMngr.sync();
+        }
+    }
 }
