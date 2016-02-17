c34e0f5a71448d7dc06b0452b2c08618855ec9b0
Ninise <nikitonze@hotmail.com>
2016-02-17 18:02:57 +0200

    Added LoginActivityTest class where i will test logic of components

--- app/src/test/java/com/ninise/pragmatictwitterclient/LoginActivityTest.java
+++ app/src/test/java/com/ninise/pragmatictwitterclient/LoginActivityTest.java
@@ -1,7 +1,43 @@
 package com.ninise.pragmatictwitterclient;
 
-/**
- * Created by ninise on 2/17/16.
- */
+import android.app.Activity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+
+import com.ninise.pragmatictwitterclient.project.mvc.view.login.LoginActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class LoginActivityTest {
+
+    private AppCompatTextView loginTextView;
+    private AppCompatButton signInButton;
+    private Activity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(LoginActivity.class);
+        signInButton = (AppCompatButton) activity.findViewById(R.id.loginSignInButton);
+        loginTextView = (AppCompatTextView) activity.findViewById(R.id.loginWelcomeTextView);
+    }
+
+    @Test
+    public void activityCreated() {
+        assertEquals(activity.getResources().getString(R.string.login_button), signInButton.getText().toString());
+    }
 }
