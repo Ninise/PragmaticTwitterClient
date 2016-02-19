ee6114087d4de17df22d7cc93dcc84b5b2510216
Ninise <nikitonze@hotmail.com>
2016-02-19 20:09:02 +0200

    Added some constants for preferences

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/utils/Constants.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/utils/Constants.java
@@ -9,6 +9,17 @@ public class Constants {
     /** SharedPreferences */
     public static final String TWITTER_PREFERENCES = "twitter_pref";
     public static final String TWITTER_NICKNAME = "nickname";
+    public static final String TWITTER_OAUTH_VERIFIER = "verifier";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String ACCESS_TOKEN_SECRET = "access_toke_secret";
+    public static final String IMAGE_URL = "image_url";
+
+    /** Broadcast */
+    public static final String AUTH_RECEIVER = "com.ninise.pratic.auth_finished";
+
+    /** AUTH DATA */
+    public static final String AUTH_URL = "auth_url";
+    public static final String AUTH_VERIFIER = "oauth_verifier";
 
 
 }
