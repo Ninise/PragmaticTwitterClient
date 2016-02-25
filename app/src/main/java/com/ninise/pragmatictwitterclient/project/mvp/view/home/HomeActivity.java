63cc6a33c7b842d10a132b0f20bad77d230cb870
Ninise <nikitonze@hotmail.com>
2016-02-25 19:42:59 +0200

    Added Navigation Drawer

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/home/HomeActivity.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/home/HomeActivity.java
@@ -1,17 +1,45 @@
 package com.ninise.pragmatictwitterclient.project.mvp.view.home;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.ninise.pragmatictwitterclient.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.homeToolBar);
+        setSupportActionBar(toolbar);
+
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+
+            if(menuItem.isChecked()) menuItem.setChecked(false);
+            else menuItem.setChecked(true);
+
+            drawerLayout.closeDrawers();
+
+            return false;
+        });
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout, toolbar, R.string.openDrawer,
+                R.string.closeDrawer);
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
         setListFragment();
     }
