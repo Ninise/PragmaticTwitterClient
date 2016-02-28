334a6d5c152ec128b55024ed94d47995c1bd76b0
Ninise <nikitonze@hotmail.com>
2016-02-28 19:59:44 +0200

    Added function for check what menu item was selected

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/home/HomeActivity.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/home/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.ninise.pragmatictwitterclient.project.mvp.view.home;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ninise.pragmatictwitterclient.R;
 import com.ninise.pragmatictwitterclient.project.mvp.model.preferences.TwitterPreferences;
@@ -19,6 +21,8 @@ public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = HomeActivity.class.getSimpleName();
     private DrawerLayout drawerLayout;
+
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,12 +35,15 @@ public class HomeActivity extends AppCompatActivity {
         final NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(menuItem -> {
 
-            if(menuItem.isChecked()) menuItem.setChecked(false);
-            else menuItem.setChecked(true);
+            if(menuItem.isChecked()) {
+                menuItem.setChecked(false);
+            } else {
+                menuItem.setChecked(true);
+            }
 
             drawerLayout.closeDrawers();
 
-            return false;
+            return menuSelected(menuItem);
         });
 
         View headView = navigationView.getHeaderView(0);
@@ -55,11 +62,42 @@ public class HomeActivity extends AppCompatActivity {
         setListFragment();
     }
 
+    private boolean menuSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.settings:
+                return true;
+            case R.id.about:
+                return true;
+            case R.id.logout:
+                closeApp();
+                return true;
+            default:
+                Toast.makeText(this, "Somethings Wrong", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+    }
+
+    private void closeApp() {
+        android.os.Process.killProcess(android.os.Process.myPid());
+        finish();
+    }
+
     private void setListFragment() {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.homeContainer, TweetsListFragment.getInstance());
         fragmentTransaction.commit();
     }
 
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            android.os.Process.killProcess(android.os.Process.myPid());
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, getString(R.string.back_pressed), Toast.LENGTH_SHORT).show();
 
+        new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000);
+    }
 }
