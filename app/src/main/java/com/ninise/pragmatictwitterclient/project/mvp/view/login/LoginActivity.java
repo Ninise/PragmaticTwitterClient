c3e478d07fdb419452e9e3a615ef3e3e189800c4
Ninise <nikitonze@hotmail.com>
2016-02-28 20:00:17 +0200

    Some changes

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/login/LoginActivity.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/login/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.ninise.pragmatictwitterclient.project.mvp.view.login;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
 
 
 import com.ninise.pragmatictwitterclient.R;
@@ -13,6 +16,7 @@ import com.ninise.pragmatictwitterclient.R;
 public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = LoginActivity.class.getName();
+    private boolean doubleBackToExitPressedOnce = false;
 
 
     @Override
@@ -32,5 +36,19 @@ public class LoginActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.loginContainer, LoginFragment.getInstance());
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            android.os.Process.killProcess(android.os.Process.myPid());
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, getString(R.string.back_pressed), Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000);
     }
 }
