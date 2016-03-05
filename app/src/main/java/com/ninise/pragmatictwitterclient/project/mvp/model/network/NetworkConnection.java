0a3dd6feb26eec4c6ac789b43ea72f0f630750ef
Ninise <nikitonze@hotmail.com>
2016-03-05 17:46:09 +0200

    Added class NetworkConnection after refactoring OAuthWorker

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/model/network/NetworkConnection.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/model/network/NetworkConnection.java
@@ -1,7 +1,34 @@
 package com.ninise.pragmatictwitterclient.project.mvp.model.network;
 
-/**
- * Created by ninise on 3/5/16.
- */
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 public class NetworkConnection {
+
+    private static NetworkConnection mInstance = null;
+
+    private Context mContext;
+
+    private NetworkConnection(Context context) {
+        mContext = context;
+    }
+
+    public static NetworkConnection getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new NetworkConnection(context);
+        }
+
+        return mInstance;
+    }
+
+    public boolean isNetworkConnectionOn() {
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+    }
+
 }
