e2cb1c473da4fe50a9ec82f9e8c2e854f5a24422
Ninise <nikitonze@hotmail.com>
2016-03-06 17:30:55 +0200

    Added test class for TwitterPreferencesAuth

--- app/src/test/java/com/ninise/pragmatictwitterclient/project/mvp/model/preferences/TwitterPreferencesAuthTest.java
+++ app/src/test/java/com/ninise/pragmatictwitterclient/project/mvp/model/preferences/TwitterPreferencesAuthTest.java
@@ -1,31 +1,57 @@
 package com.ninise.pragmatictwitterclient.project.mvp.model.preferences;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.ninise.pragmatictwitterclient.BuildConfig;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowPreference;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by ninise on 3/6/16.
- */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class TwitterPreferencesAuthTest {
+
+    ShadowActivity activity = new ShadowActivity();
+
+    private TwitterPreferencesAuth mPreferencesAuth;
+
+    final String testAccessToken = "2321353123gfd1sad14";
+    final String testAccessSecret = "sddr3fas2144gs3413";
+
+    @Before
+    public void setUp() {
+        mPreferencesAuth = TwitterPreferencesAuth.getInstance(activity.getApplicationContext());
+        mPreferencesAuth.setOAuthAccessTokenAndSecret(testAccessToken, testAccessSecret);
+    }
 
     @Test
     public void testGetInstance() throws Exception {
-
+        Assertions.assertThat(mPreferencesAuth).isNotNull();
     }
 
     @Test
     public void testSetOAuthAccessTokenAndSecret() throws Exception {
-
+        Assertions.assertThat(mPreferencesAuth.setOAuthAccessTokenAndSecret(testAccessToken, testAccessSecret)).isTrue();
     }
 
     @Test
     public void testGetOAuthAccessToken() throws Exception {
-
+        Assertions.assertThat(mPreferencesAuth.getOAuthAccessToken()).isEqualTo(testAccessToken);
     }
 
     @Test
     public void testGetOAuthAccessTokenSecret() throws Exception {
-
+        Assertions.assertThat(mPreferencesAuth.getOAuthAccessTokenSecret()).isEqualTo(testAccessSecret);
     }
 }
