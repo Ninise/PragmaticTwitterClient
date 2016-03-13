106caeef21039430f992f86b435b0427dd969800
Ninise <nikitonze@hotmail.com>
2016-03-13 19:42:37 +0200

    Now user can to send tweet

---
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/model/network/data/PostTweet.java
@@ -0,0 +1,44 @@
+package com.ninise.pragmatictwitterclient.project.mvp.model.network.data;
+
+
+import android.content.Context;
+
+import com.ninise.pragmatictwitterclient.project.mvp.model.preferences.TwitterPreferencesAuth;
+import com.ninise.pragmatictwitterclient.project.utils.Constants;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class PostTweet {
+
+    public static Observable<Boolean> setStatus(Context context, String post) {
+        return Observable.create((Observable.OnSubscribe<Boolean>) subscriber -> {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.setOAuthConsumerKey(Constants.TWITTER_KEY);
+            builder.setOAuthConsumerSecret(Constants.TWITTER_SECRET);
+
+            AccessToken accessToken = new AccessToken(
+                    TwitterPreferencesAuth.getInstance(context).getOAuthAccessToken(),
+                    TwitterPreferencesAuth.getInstance(context).getOAuthAccessTokenSecret()
+            );
+
+            Twitter twitter = new TwitterFactory(builder.build()).getInstance(accessToken);
+
+            try {
+                twitter.updateStatus(post);
+            } catch (TwitterException e) {
+                e.printStackTrace();
+            }
+
+            subscriber.onNext(true);
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+}
---
+++ app/src/test/java/com/ninise/pragmatictwitterclient/project/mvp/model/network/data/PostTweetTest.java
@@ -0,0 +1,16 @@
+package com.ninise.pragmatictwitterclient.project.mvp.model.network.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ninise on 3/13/16.
+ */
+public class PostTweetTest {
+
+    @Test
+    public void testSetStatus() throws Exception {
+
+    }
+}
