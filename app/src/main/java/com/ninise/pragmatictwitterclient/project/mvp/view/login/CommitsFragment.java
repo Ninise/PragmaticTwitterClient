08a1d945fc7c06fc394270588e10b20a95523b07
Ninise <nikitonze@hotmail.com>
2016-03-16 21:13:25 +0200

    Commits fragment where contains all latest commits was added

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/login/CommitsFragment.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/login/CommitsFragment.java
@@ -1,7 +1,52 @@
 package com.ninise.pragmatictwitterclient.project.mvp.view.login;
 
-/**
- * Created by ninise on 3/16/16.
- */
-public class CommitsFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ninise.pragmatictwitterclient.R;
+import com.ninise.pragmatictwitterclient.project.mvp.model.network.data.github.GetUpdates;
+import com.ninise.pragmatictwitterclient.project.mvp.presenter.adapters.CommitAdapter;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CommitsFragment extends Fragment {
+
+    @Bind(R.id.rv) RecyclerView mRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_recycler, container, false);
+        ButterKnife.bind(this, v);
+
+        final LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(llm);
+        mRecyclerView.setHasFixedSize(true);
+
+        final RecyclerView.ItemAnimator itemAnimator = new DefaultItemAnimator();
+        itemAnimator.setAddDuration(1000);
+        itemAnimator.setRemoveDuration(1000);
+        mRecyclerView.setItemAnimator(itemAnimator);
+
+        GetUpdates.getCommits().subscribe(pojos -> {
+            CommitAdapter adapter = new CommitAdapter(pojos);
+            mRecyclerView.setAdapter(adapter);
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        ButterKnife.unbind(this);
+        super.onDestroy();
+    }
 }
