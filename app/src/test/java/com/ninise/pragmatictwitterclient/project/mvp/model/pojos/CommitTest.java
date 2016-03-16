32b1187c518f4c9a000239dfe7e5aeb58798db3c
Ninise <nikitonze@hotmail.com>
2016-03-16 19:36:26 +0200

    Test class for Commit was created

--- app/src/test/java/com/ninise/pragmatictwitterclient/project/mvp/model/pojos/CommitTest.java
+++ app/src/test/java/com/ninise/pragmatictwitterclient/project/mvp/model/pojos/CommitTest.java
@@ -1,21 +1,29 @@
 package com.ninise.pragmatictwitterclient.project.mvp.model.pojos;
 
+import com.ninise.pragmatictwitterclient.BuildConfig;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by ninise on 3/16/16.
- */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class CommitTest {
+
+    private String testStr = "SomeString";
+    private Commit mCommit = new Commit();
 
     @Test
     public void testGetMessage() throws Exception {
-
+        mCommit.setMessage(testStr);
+        Assertions.assertThat(mCommit.getMessage()).isEqualToIgnoringCase(testStr);
     }
 
     @Test
     public void testSetMessage() throws Exception {
-
+        mCommit.setMessage(testStr);
+        Assertions.assertThat(mCommit.getMessage()).isEqualToIgnoringCase(testStr);
     }
 }
