6ea3a80c3b404a603e8c730505433c24bf129dca
Ninise <nikitonze@hotmail.com>
2016-03-16 19:35:54 +0200

    Test class for GetUpdates was created

--- app/src/test/java/com/ninise/pragmatictwitterclient/project/mvp/model/network/data/github/GetUpdatesTest.java
+++ app/src/test/java/com/ninise/pragmatictwitterclient/project/mvp/model/network/data/github/GetUpdatesTest.java
@@ -1,16 +1,24 @@
 package com.ninise.pragmatictwitterclient.project.mvp.model.network.data.github;
 
+import com.ninise.pragmatictwitterclient.BuildConfig;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.*;
+import rx.observers.TestSubscriber;
 
-/**
- * Created by ninise on 3/16/16.
- */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class GetUpdatesTest {
 
     @Test
     public void testGetCommits() throws Exception {
+        TestSubscriber subscriber = new TestSubscriber();
 
+        GetUpdates.getCommits().subscribe(subscriber);
+        subscriber.assertNoErrors();
     }
 }
