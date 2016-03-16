2e82a0c6ce652202dc99b50f5861cc3370edf415
Ninise <nikitonze@hotmail.com>
2016-03-16 21:12:10 +0200

    Viewholder for recycler view was added

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/model/viewholders/CommitViewHolder.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/model/viewholders/CommitViewHolder.java
@@ -1,7 +1,22 @@
 package com.ninise.pragmatictwitterclient.project.mvp.model.viewholders;
 
-/**
- * Created by ninise on 3/16/16.
- */
-public class CommitViewHolder {
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ninise.pragmatictwitterclient.R;
+
+public class CommitViewHolder extends RecyclerView.ViewHolder {
+
+    final public CardView cvCommit;
+    final public TextView CommitMessageTextView;
+    final public TextView CommitTimeTextView;
+
+    public CommitViewHolder(View itemView) {
+        super(itemView);
+        cvCommit = (CardView) itemView.findViewById(R.id.cvCommit);
+        CommitMessageTextView = (TextView) itemView.findViewById(R.id.cardViewCommitMessage);
+        CommitTimeTextView = (TextView) itemView.findViewById(R.id.loginLatestUpdates);
+    }
 }
