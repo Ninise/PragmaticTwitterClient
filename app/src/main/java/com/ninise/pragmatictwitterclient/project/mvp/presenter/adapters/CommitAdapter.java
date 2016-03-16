0d47bdeb977883ca84017533271556f6a5c6caff
Ninise <nikitonze@hotmail.com>
2016-03-16 21:12:37 +0200

    Adapter for recycler view was added

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/presenter/adapters/CommitAdapter.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/presenter/adapters/CommitAdapter.java
@@ -1,7 +1,36 @@
 package com.ninise.pragmatictwitterclient.project.mvp.presenter.adapters;
 
-/**
- * Created by ninise on 3/16/16.
- */
-public class CommitAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.ninise.pragmatictwitterclient.R;
+import com.ninise.pragmatictwitterclient.project.mvp.model.pojos.Pojo;
+import com.ninise.pragmatictwitterclient.project.mvp.model.viewholders.CommitViewHolder;
+
+import java.util.List;
+
+public class CommitAdapter extends RecyclerView.Adapter<CommitViewHolder> {
+
+    private List<Pojo> mDataSet;
+
+    public CommitAdapter(List<Pojo> dataSet) {
+        mDataSet = dataSet;
+    }
+
+    @Override
+    public CommitViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new CommitViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cardview_commit_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(CommitViewHolder holder, int position) {
+        holder.CommitMessageTextView.setText(mDataSet.get(holder.getAdapterPosition()).getCommit());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
 }
