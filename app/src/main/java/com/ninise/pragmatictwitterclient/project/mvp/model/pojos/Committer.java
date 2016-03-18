2a40bace34cfb2629f5a24277ad58ea860e8768e
Ninise <nikitonze@hotmail.com>
2016-03-18 19:00:39 +0200

    Class Committer was added for geting date of commit

---
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/model/pojos/Committer.java
@@ -0,0 +1,16 @@
+package com.ninise.pragmatictwitterclient.project.mvp.model.pojos;
+
+
+public class Committer {
+
+    private String date;
+
+    public String getDate() {
+        date = date.replaceAll("[^:\\d-]", " ");
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
