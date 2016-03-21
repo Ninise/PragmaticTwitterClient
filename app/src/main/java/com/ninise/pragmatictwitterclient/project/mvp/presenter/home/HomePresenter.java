0c59a77c72a66aff93c94b662d3d15bc3615ebd5
Ninise <nikitonze@hotmail.com>
2016-03-21 19:25:54 +0200

    Presenter for HomeActivity was added

---
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/presenter/home/HomePresenter.java
@@ -0,0 +1,34 @@
+package com.ninise.pragmatictwitterclient.project.mvp.presenter.home;
+
+
+import android.content.Context;
+
+import com.ninise.pragmatictwitterclient.project.mvp.model.network.data.twitter.PostTweet;
+import com.ninise.pragmatictwitterclient.project.mvp.model.network.data.twitter.ProfileImage;
+
+import java.net.MalformedURLException;
+
+public class HomePresenter implements IHomePresenter {
+
+    private IHomeView mView;
+
+    public HomePresenter(IHomeView view) {
+        this.mView = view;
+    }
+
+    @Override
+    public void postTweet(Context context, String tweet) {
+        PostTweet.setStatus(context, tweet)
+                .subscribe((aVoid) -> mView.postTweeted(), (aVoid) -> mView.postTweetError());
+    }
+
+    @Override
+    public void getProfilePhoto(Context context) {
+        try {
+            ProfileImage.getProfileImage(context)
+                    .subscribe(mView::setProfileIcon);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/presenter/home/IHomeView.java
@@ -0,0 +1,7 @@
+package com.ninise.pragmatictwitterclient.project.mvp.presenter.home;
+
+/**
+ * Created by ninise on 21.03.16.
+ */
+public interface IHomeView {
+}
