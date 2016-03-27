a62677dbcb7b3a6110fa1e5336f4566311c85199
Ninise <nikitonze@hotmail.com>
2016-03-27 18:13:11 +0300

    Preferences for Settings added

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/model/preferences/settings/SettingsPreferences.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/model/preferences/settings/SettingsPreferences.java
@@ -1,7 +1,37 @@
 package com.ninise.pragmatictwitterclient.project.mvp.model.preferences.settings;
 
-/**
- * Created by ninise on 27.03.16.
- */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.ninise.pragmatictwitterclient.project.utils.Constants;
+
 public class SettingsPreferences {
+
+    private static SettingsPreferences mInstance = null;
+
+    private SharedPreferences mPreferences;
+
+    private SettingsPreferences(Context context) {
+        mPreferences = context.getSharedPreferences(Constants.SETTINGS_PREFERENCES, Context.MODE_PRIVATE);
+    }
+
+    public static SettingsPreferences getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new SettingsPreferences(context);
+        }
+
+        return mInstance;
+    }
+
+    public boolean setGithubUpdatesStatus(boolean checked) {
+        SharedPreferences.Editor editor = mPreferences.edit();
+        editor.putBoolean(Constants.SETTINGS_GITUPDATES, checked);
+        return editor.commit();
+    }
+
+    public boolean getGithugUpdatesStatus() {
+        return mPreferences.contains(Constants.SETTINGS_GITUPDATES)
+                && mPreferences.getBoolean(Constants.SETTINGS_GITUPDATES, false);
+    }
 }
