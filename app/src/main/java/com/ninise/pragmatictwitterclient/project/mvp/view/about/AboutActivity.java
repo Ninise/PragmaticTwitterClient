55e4274609220335498e028011d489b3645d26d6
Ninise <nikitonze@hotmail.com>
2016-03-28 19:02:49 +0300

    Laucher menu icon changed

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/about/AboutActivity.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/mvp/view/about/AboutActivity.java
@@ -7,22 +7,27 @@ import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.jakewharton.rxbinding.support.v7.widget.RxToolbar;
 import com.jakewharton.rxbinding.view.RxView;
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.ninise.pragmatictwitterclient.R;
+import com.ninise.pragmatictwitterclient.project.mvp.presenter.about.AboutPresenter;
+import com.ninise.pragmatictwitterclient.project.mvp.presenter.about.IAboutView;
 
 import butterknife.Bind;
 import butterknife.BindDrawable;
 import butterknife.BindString;
 import butterknife.ButterKnife;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements IAboutView {
 
     @Bind(R.id.aboutToolbar) Toolbar mAboutToolbar;
+    @Bind(R.id.toolbarIcon) CircularImageView mToolbarIconCircularImageView;
     @Bind(R.id.toolbarTitle) TextView mToolbarTitleTextView;
     @Bind(R.id.aboutOkButton) Button mOkButton;
+    @BindDrawable(R.drawable.app_logo) Drawable mToolbarIconDrawable;
     @BindString(R.string.about) String mToolbarTitleString;
+
+    private AboutPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,24 +40,25 @@ public class AboutActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         mAboutToolbar.setNavigationIcon(R.drawable.ic_action_hardware_keyboard_backspace);
-        mAboutToolbar.setNavigationOnClickListener(v -> leaveFromActivity());
+        mAboutToolbar.setNavigationOnClickListener(v -> mPresenter.backToHome());
 
+        mToolbarIconCircularImageView.setImageDrawable(mToolbarIconDrawable);
         mToolbarTitleTextView.setText(mToolbarTitleString);
 
-        RxView.clicks(mOkButton).subscribe(v -> {
+        RxView.clicks(mOkButton).subscribe(v -> mPresenter.backToHome());
 
-            leaveFromActivity();
-        });
-    }
-
-    private void leaveFromActivity() {
-        onBackPressed();
-        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+        mPresenter = new AboutPresenter(this);
     }
 
     @Override
     protected void onDestroy() {
         ButterKnife.unbind(this);
         super.onDestroy();
+    }
+
+    @Override
+    public void onBackToHome() {
+        onBackPressed();
+        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
     }
 }
