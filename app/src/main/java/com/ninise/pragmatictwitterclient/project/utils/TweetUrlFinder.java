2f177f369d018909b54c52ba7a964a277a89d4ab
Ninise <nikitonze@hotmail.com>
2016-04-01 19:16:08 +0300

    Class for find urls in tweets added

--- app/src/main/java/com/ninise/pragmatictwitterclient/project/utils/TweetUrlFinder.java
+++ app/src/main/java/com/ninise/pragmatictwitterclient/project/utils/TweetUrlFinder.java
@@ -1,7 +1,26 @@
 package com.ninise.pragmatictwitterclient.project.utils;
 
-/**
- * Created by ninise on 01.04.16.
- */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TweetUrlFinder {
+
+    private static final Pattern urlPattern = Pattern.compile(
+            "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)"
+                    + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
+                    + "[\\p{Alnum}.,%_=?&#\\-+()\\[\\]\\*$~@!:/{};']*)",
+            Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+
+    public static String getUrl(String text) {
+        Matcher matcher = urlPattern.matcher(text);
+        int matchStart = 0;
+        int matchEnd = 0;
+        while (matcher.find()) {
+            matchStart = matcher.start();
+            matchEnd = matcher.end();
+        }
+
+        return text.substring(matchStart, matchEnd);
+    }
 }
---
+++ app/src/test/java/com/ninise/pragmatictwitterclient/project/utils/TweetUrlFinderTest.java
@@ -0,0 +1,13 @@
+package com.ninise.pragmatictwitterclient.project.utils;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by ninise on 01.04.16.
+ */
+public class TweetUrlFinderTest extends TestCase {
+
+    public void testGetUrl() throws Exception {
+
+    }
+}
