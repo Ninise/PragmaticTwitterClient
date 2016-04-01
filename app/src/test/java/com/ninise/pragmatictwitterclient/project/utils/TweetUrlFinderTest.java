c44e7b4c754bec6075188f410aa935edc50a9878
Ninise <nikitonze@hotmail.com>
2016-04-01 19:16:31 +0300

    Test class for url finder added

--- app/src/test/java/com/ninise/pragmatictwitterclient/project/utils/TweetUrlFinderTest.java
+++ app/src/test/java/com/ninise/pragmatictwitterclient/project/utils/TweetUrlFinderTest.java
@@ -2,12 +2,21 @@ package com.ninise.pragmatictwitterclient.project.utils;
 
 import junit.framework.TestCase;
 
-/**
- * Created by ninise on 01.04.16.
- */
+import org.assertj.core.api.Assertions;
+
 public class TweetUrlFinderTest extends TestCase {
 
     public void testGetUrl() throws Exception {
+        Assertions.assertThat(TweetUrlFinder.getUrl("asdasd213 https://t.co/ygpJ2Mc7gr. asdasdas"))
+                .isNotNull();
 
+        Assertions.assertThat(TweetUrlFinder.getUrl("123http://google.com.ua alalal"))
+                .isNotNull();
+
+        Assertions.assertThat(TweetUrlFinder.getUrl("asdasd213 https://t.co/ygpJ2Mc7gr asdasdas"))
+                .isEqualToIgnoringCase(" https://t.co/ygpJ2Mc7gr");
+
+        Assertions.assertThat(TweetUrlFinder.getUrl("123 http://google.com.ua alalal"))
+                .isEqualToIgnoringCase(" http://google.com.ua");
     }
 }
